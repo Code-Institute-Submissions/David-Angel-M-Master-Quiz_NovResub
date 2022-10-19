74657b07692c778dae84b24de781d0c4828c99a9
David-Angel-M <david.angelm28@gmail.com>
2022-10-19 23:09:36 +0000

    Add some style to main body and a short paragraph on the readme file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,177 @@
 let questions = [
     {
-        prompt: "1. Who was the lead singer of Nirvana?\n(a) Dave Grohl(b) Kurt Cobain\n(c) James Hetfield",
+        prompt: "1. Who was the lead singer of Nirvana?",
+        a: "Dave Grohl",
+        b: "Kurt Cobain",
+        c: "James Hetfield",
         answer: "b"
     },
     {
-        prompt: "2. What heavy rock festival takes place every year at Donnington Park?\n(a) UK Rock\n(b) Glastonbury\n(c) Download",
-        answer:"c"
+        prompt: "2. What heavy rock festival takes place every year at Donnington Park?",
+        a: "UK Rock",
+        b: "Glastonbury",
+        c: "Download",
+        answer: "c"
     },
     {
-        prompt: "3. Which group had the most UK number one singles in the 1960s?\n(a) The Animals\n(b) The Beatles\n(c) The Birds",
-        answer:"b"
+        prompt: "3. Which group had the most UK number one singles in the 1960s?",
+        a: "The Animals",
+        b: "The Beatles",
+        c: "The Birds",
+        answer: "b"
     },
     {
-        prompt: "4. What colour was the Little Rooster according to the title of a Rolling Stones hit?\n(a) Red\n(b) Red/Blue\n(c) White",
-        answer:"a"
+        prompt: "4. What colour was the Little Rooster according to the title of a Rolling Stones hit?",
+        a: "Red",
+        b: "Red/Blue",
+        c: "White",
+        answer: "a"
     },
     {
-        prompt: "5. Gordon Sumner is the real name of which musician?\n(a) Jim Morrison\n(b) Sting\n(c) Bruno Mars",
-        answer:"b"
+        prompt: "5. Gordon Sumner is the real name of which musician?",
+        a: "Jim Morrison",
+        b: "Sting",
+        c: "Bruno Mars",
+        answer: "b"
     },
     {
-        prompt: "6. Which rapper had a hit with 'Gangster's Paradise' in 1995?\n(a) Biggie Smalls\n(b) 2Pac\n(c) Coolio",
-        answer:"c"
+        prompt: "6. Which rapper had a hit with 'Gangster's Paradise' in 1995?",
+        a: "Biggie Smalls",
+        b: "2Pac",
+        c: "Coolio",
+        answer: "c"
     },
     {
-        prompt: "7. 'Maybe I'm foolish, maybe I'm blind' is a lyric by which artist?\n(a) Rag'n'Bone Man\n(b) Adele\n(c) Harry Styles",
-        answer:"a"
+        prompt: "7. 'Maybe I'm foolish, maybe I'm blind' is a lyric by which artist?",
+        a: "Rag'n'Bone Man",
+        b: "Adele",
+        c: "Harry Styles",
+        answer: "a"
     },
     {
-        prompt: "8. Which member of The Beatles formed Wings?/n(a) John Lennon\n(b) Paul McCartney\n(c) Ringo Star",
-        answer:"b"
+        prompt: "8. Which member of The Beatles formed Wings?",
+        a: "John Lennon",
+        b: "Paul McCartney",
+        c: "Ringo Star",
+        answer: "b"
     },
     {
-        prompt: "9. Ozzy Osbourne was the lead singer for which heavy metal band?\n(a) Iron Maiden\n(b) Motörhead\n(c) Black Sabbath",
-        answer:"a"
+        prompt: "9. Ozzy Osbourne was the lead singer for which heavy metal band?",
+        a: "Iron Maiden",
+        b: "Motörhead",
+        c: "Black Sabbath",
+        answer: "a"
     },
     {
-        prompt: "10. Which song has the lyric “This hit, that ice cold, Michelle Pfeiffer, that white gold”?\n(a) Lose Yourself To Dance\n(b) Look At Me Now\n(c) Uptown Funk",
-        answer:"c"
+        prompt: "10. Which song has the lyric “This hit, that ice cold, Michelle Pfeiffer, that white gold”?",
+        a: "Lose Yourself To Dance",
+        b: "Look At Me Now",
+        c: "Uptown Funk",
+        answer: "c"
     },
     {
-        prompt: "11. What nationality is Drake?\n(a) American\n(b) British\n(c) Canadian",
-        answer:"c"
+        prompt: "11. What nationality is Drake?",
+        a: "American",
+        b: "British",
+        c: "Canadian",
+        answer: "c"
     },
     {
-        prompt: "12. Which nation won the Eurovision Song Contest 2022?\n(a) Ukraine\n(b) Lithuania\n(c) Estonia",
-        answer:"a"
+        prompt: "12. Which nation won the Eurovision Song Contest 2022?",
+        a: "Ukraine",
+        b: "Lithuania",
+        c: " Estonia",
+        answer: "a"
     },
     {
-        prompt: "13. According to Oasis, who knows it's too late?\n(a) Molly\n(b) Sally\n(c) Sully",
-        answer:"b"
+        prompt: "13. According to Oasis, who knows it's too late?",
+        a: "Molly",
+        b: "Sally",
+        c: "Sully",
+        answer: "b"
     },
     {
-        prompt: "14. Who had hits with 'Johnny B. Goode' and 'Roll Over Beethoven'?\n(a) Elvis Presley\n(b) Chubby Checker\n(c) Chuck Berry",
-        answer:"c"
+        prompt: "14. Who had hits with 'Johnny B. Goode' and 'Roll Over Beethoven'?",
+        a: "Elvis Presley",
+        b: "Chubby Checker",
+        c: "Chuck Berry",
+        answer: "c"
     },
     {
-        prompt: "15. Arctic Monkeys formed in which UK city?\n(a) Reading\n(b) Sheffield\n(c) Leeds",
-        answer:"b"
+        prompt: "15. Arctic Monkeys formed in which UK city?",
+        a: "Reading",
+        b: "Sheffield",
+        c: " Leeds",
+        answer: "b"
     },
     {
-        prompt: "16. What was Elvis Presley's home called?\n(a) Neverland\n(b) Dreamland\n(c) Graceland",
-        answer:"c"
+        prompt: "16. What was Elvis Presley's home called?",
+        a: "Neverland",
+        b: "Dreamland",
+        c: " Graceland",
+        answer: "c"
     },
     {
-        prompt: "17. K-pop is a genre which refers to pop music from which country?\n(a) South Korea\n(b) Kenya\n(c) Korea",
-        answer:"a"
+        prompt: "17. K-pop is a genre which refers to pop music from which country?",
+        a: "South Korea",
+        b: "Kenya",
+        c: "Korea",
+        answer: "a"
     },
     {
-        prompt: "18. In David Bowie's 'Space Oddity', who are ground control trying to contact?\n(a) Sargent Paul\n(b) Major Tom\n(c) Captain Rod",
-        answer:"b"
+        prompt: "18. In David Bowie's 'Space Oddity', who are ground control trying to contact?",
+        a: "Sargent Paul",
+        b: "Major Tom",
+        c: " Captain Rod",
+        answer: "b"
     },
     {
-        prompt: "19. “I got sunshine on a cloudy day” is the opening line to which song by The Temptations?\n(a)  Feel This Way\n(b) Sunshine\n(c) My Girl",
-        answer:"c"
+        prompt: "19. “I got sunshine on a cloudy day” is the opening line to which song by The Temptations?",
+        a: "Feel This Way",
+        b: "Sunshine",
+        c: "My Girl",
+        answer: "c"
     },
     {
-        prompt: "20. In what year did The Weather Girls have a hit with 'It's Raining Men'?\n(a) 1982\n(b) 1981\n(c) 1978",
-        answer:"a"
+        prompt: "20. In what year did The Weather Girls have a hit with 'It's Raining Men'?",
+        a: "1982",
+        b: "1981",
+        c: "1978",
+        answer: "a"
     },
  
 ]
 let score = 0;
+let currentQuestionIndex = 0;
+let currentQuestion = questions[currentQuestionIndex];
+let optionButtons = document.getElementsByClassName('option');
 
-for (let i = 0; i < questions.length; i++) {
-  let response = window.prompt(questions[i].prompt); 
-  if(response == questions[i].answer){
-    score++;
-    alert("Correct!");
-  } else {
-    alert("WRONG!");
-  }
+function showQuestion(question) {
+    document.getElementById('question').innerText = question.prompt;
+    document.getElementById('option1').innerText = question.a;
+    document.getElementById('option2').innerText = question.b;
+    document.getElementById('option3').innerText = question.c;
 }
-alert("You got " + score + "/" + questions.length);
+
+function validateAnswer(event){
+    const userAnswer = event.target.getAttribute('data-value');
+    console.log('user answer', userAnswer);
+
+    if (userAnswer === currentQuestion.answer){
+        console.log("Correct");
+        score = score + 1;
+        document.getElementById('score').innerText = score;
+    }else{
+        console.log("Incorrect");
+    }
+    currentQuestionIndex = currentQuestionIndex + 1;
+    currentQuestion = questions[currentQuestionIndex];
+    showQuestion(currentQuestion);
+}
+
+
+for (const button of optionButtons){
+    button.addEventListener('click', validateAnswer)
+}
+
+showQuestion(currentQuestion);
